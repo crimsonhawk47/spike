880a6188013aa30aa5ab5825a47a927ac84da7b5
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-09 13:53:41 -0600

    Socket MEssages Working

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-// Route includes
-const favoriteRouter = require('./routes/favorite.router');
-const categoryRouter = require('./routes/category.router');
+const socket = require('socket.io')
+
+
+// // Route includes
+// const favoriteRouter = require('./routes/favorite.router');
+// const categoryRouter = require('./routes/category.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -13,14 +16,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files
 app.use(express.static('build'));
 
-/* Routes */
-app.use('/api/favorite', favoriteRouter);
-app.use('/api/category', categoryRouter);
+
+
+// /* Routes */
+// app.use('/api/favorite', favoriteRouter);
+// app.use('/api/category', categoryRouter);
 
 // App Set //
 const PORT = process.env.PORT || 5000;
 
 /** Listen * */
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
+});
+
+io = socket(server)
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+
+  socket.on('SEND_MESSAGE', function (data) {
+    io.emit('RECEIVE_MESSAGE', data);
+  })
 });
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,13 +1,74 @@
-import React, {Component} from 'react';
-class Chat extends Component{
- 
-    render(){
-        return(
-        <div>
-            I am chat
-        </div>
-        )
+import React, { Component } from 'react';
+import io from 'socket.io-client'
+class Chat extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            message: '',
+            messages: []
+        }
+
+        console.log(this.state);
+        
+
+        this.socket = io('localhost:5000');
+
+        this.socket.on('RECEIVE_MESSAGE', function(data){
+            addMessage(data)
+            
+            
+        })
+
+        const addMessage = (data) => {
+            this.setState({
+                messages: [...this.state.messages, data]
+            })
+        }
+
+        
+    }
+
     
+
+    componentDidMount(){
+        console.log(this.state);
+        
+        
+    }
+
+    // state = {
+    //     username: '',
+    //     message: '',
+    //     messages: []
+    // }
+
+    sendMessage = ev => {
+        ev.preventDefault();
+        this.socket.emit('SEND_MESSAGE', {
+            author: this.state.username,
+            message: this.state.message
+        });
+        this.setState({message: ''});
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" placeholder="Username" value={this.state.username} onChange={event => this.setState({ username: event.target.value })} />
+                <br />
+                <input type="text" placeholder="Message" value={this.state.message} onChange={event => this.setState({ message: event.target.value })} />
+                <br />
+                <button onClick={this.sendMessage}>Send</button>
+                {this.state.messages.map(message => {
+                    return <p>{message.message}</p>
+                })}
+            </div>
+        )
+
+
     }
 }
 export default Chat
