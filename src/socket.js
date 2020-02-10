eabdd6d9a15d062b8dd6e8abb79e48fd0f057f74
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-09 20:31:05 -0600

    Made Socket Connection Default To Domain

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import store from './store'
 
 import io from 'socket.io-client'
 
-const socket = io('192.168.10.107:5000');
+const socket = io();
 
 socket.on('SOCKET_MESSAGES', (data) => {
     console.log(`LOGGING DATA`);
